b60d874d386059bc915191d152ec2286f9fb3254
Huzaifa Ansari <huzaifaks123@gmail.com>
2024-11-22 16:37:59 +0530

    product render

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, } from 'react-native'
 import products from '../assets/products.json'
-import ProductListItems from '../components/ProductListItems'
-import { Button, ButtonText } from '@/components/ui/button'
+import ProductListItems from '@/components/ProductListItems'
 
 const HomeScreen = () => {
-  return (
-    <Button>
-      <ButtonText>
-        Hello World!
-      </ButtonText>
-    </Button>
-  )
   // return (
-  //   <FlatList 
-  //   data={products}
-  //   renderItem={({item}) => 
-  //   <ProductListItems product={item}/>
-  //   }
-  //   />
+  //   <Button>
+  //     <ButtonText>
+  //       Hello World!
+  //     </ButtonText>
+  //   </Button>
   // )
+  return (
+    <FlatList 
+    data={products}
+    numColumns={2}
+    contentContainerClassName='gap-2'
+    columnWrapperClassName='gap-2'
+    renderItem={({item}) => 
+    <ProductListItems product={item}/>
+    }
+    />
+  )
 }
 
 export default HomeScreen 
